472404c83d51ee77daae459e21e46e54b5348894
Denislav Kanev <denk@itu.dk>
2019-04-19 10:03:27 +0300

    Created the commands to handle the presence of the users, login, logoff and notifications for other users

--- ChatServer/src/kanev/denislav/ServerMain.java
+++ ChatServer/src/kanev/denislav/ServerMain.java
@@ -1,27 +1,14 @@
 package kanev.denislav;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 /**
  * Created by Denislav on 4/18/2019.
  */
 public class ServerMain {
     public static void main(String[] args){
+
         int port = 8810;
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            while(true) {
-                System.out.println("About to accept connection:...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from " + clientSocket);
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server(port);
+        server.start();
     }
 
 
--- ChatServer/src/kanev/denislav/ServerWorker.java
+++ ChatServer/src/kanev/denislav/ServerWorker.java
@@ -2,6 +2,7 @@ package kanev.denislav;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.List;
 
 
 /**
@@ -10,9 +11,12 @@ import java.net.Socket;
 public class ServerWorker extends Thread {
 
     private final Socket clientSocket;
+    private final Server server;
     private String login = null;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket) {
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
         this.clientSocket = clientSocket;
     }
 
@@ -27,7 +31,7 @@ public class ServerWorker extends Thread {
 
     private void handleClientSocket() throws IOException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -35,7 +39,8 @@ public class ServerWorker extends Thread {
             String[] tokens = line.split(" ");
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(cmd)) {
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
+                    handleLogoff();
                     break;
                 }else if("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
@@ -49,6 +54,23 @@ public class ServerWorker extends Thread {
         clientSocket.close();
     }
 
+    private void handleLogoff() throws IOException {
+        List<ServerWorker> workerList = server.getWorkerList();
+
+        // send other online users current user's status
+        String onlineMsg = "offline " + login + System.lineSeparator();
+        for(ServerWorker worker : workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+        clientSocket.close();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
     private void handleLogin(OutputStream outputStream, String[] tokens) {
         if (tokens.length == 3) {
             String login = tokens[1];
@@ -60,6 +82,30 @@ public class ServerWorker extends Thread {
                     outputStream.write(msg.getBytes());
                     this.login = login;
                     System.out.println("User logged in successfully: " + login);
+
+
+                    List<ServerWorker> workerList = server.getWorkerList();
+
+                    // send current user all other online logins
+                    for(ServerWorker worker : workerList) {
+
+                            if (worker.getLogin() != null) {
+                                if (!login.equals(worker.getLogin())) {
+                                    String msg2 = "online " + worker.getLogin() + System.lineSeparator();
+                                    send(msg2);
+                                }
+                            }
+
+                    }
+
+                    // send other online users current user's status
+                    String onlineMsg = "online " + login + System.lineSeparator();
+                    for(ServerWorker worker : workerList) {
+                        if (!login.equals(worker.getLogin())) {
+                            worker.send(onlineMsg);
+                        }
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -74,5 +120,11 @@ public class ServerWorker extends Thread {
 
             }
         }
+
+    private void send(String msg) throws IOException {
+
+            outputStream.write(msg.getBytes());
+
     }
+}
 
